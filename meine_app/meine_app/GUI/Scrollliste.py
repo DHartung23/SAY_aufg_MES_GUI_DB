98436e20afb6a388402293c8d46525a4d57055ad
Timo <timoreinhold2912@gmail.com>
2025-05-20 14:45:02 +0200

    SCANASimulator.py

---
+++ meine_app/meine_app/GUI/Scrollliste.py
@@ -0,0 +1,12 @@
+from kivy.uix.scrollview import ScrollView
+
+
+class Scrollliste(ScrollView):
+    def __init__(self, auswahl, beiAuswahl, *args):
+        pass
+
+    def get_auswahl(self):
+        pass
+
+    def add_element(self, *args):
+        pass

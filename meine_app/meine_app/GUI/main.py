0c5213f56b1b11c1d2f2313b12edbf65efbb4184
Timo <timoreinhold2912@gmail.com>
2025-05-26 21:50:58 +0200

    digga was für message

--- meine_app/meine_app/GUI/main.py
+++ meine_app/meine_app/GUI/main.py
@@ -6,6 +6,8 @@ from home_screen import HomeScreen
 from PrdAuftr_screen import PrdAuftragScreen
 #from meine_app.meine_app.test import ScrollButtonApp
 from PrdAuftrView_screen import PrdAuftrViewScreen
+from PrdLinie_screen import PrdLinieScreen
+
 # Load .kv files
 Builder.load_file("../kv/login_screen.kv")
 Builder.load_file("../kv/home_screen.kv")
@@ -17,6 +19,7 @@ class MainApp(MDApp):
         sm.add_widget(HomeScreen(name="home"))
         sm.add_widget(PrdAuftragScreen(name="prdauftrag"))
         sm.add_widget(PrdAuftrViewScreen(name="prdauftragView"))
+        sm.add_widget(PrdLinieScreen(name="prdlinie"))
 
         return sm
 
